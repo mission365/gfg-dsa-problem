d653e03ebf5c55992ae479bd941dced73a52acee
Mission Devnath <117886802+mission365@users.noreply.github.com>
2024-11-12 15:27:37 +0600

    gfg

---
+++ arrays/Next_Permutation.cpp
@@ -0,0 +1,38 @@
+// User function Template for C++
+
+
+// solution: 1
+// class Solution {
+//   public:
+//     void nextPermutation(vector<int>& arr) {
+//         // code here
+//         next_permutation(arr.begin(),arr.end());
+//     }
+// };
+
+
+class Solution {
+  public:
+    void nextPermutation(vector<int>& arr) {
+        // code here
+        int n = arr.size();
+        int index = -1;
+        for(int i=n-2; i>=0;i--){
+            if(arr[i]<arr[i+1]){
+                index=i;
+                break;
+            }
+        }
+        if(index == -1){
+            reverse(arr.begin(), arr.end());
+            return;
+        }
+        for(int i=n-1;i>=index;i--){
+            if(arr[i]> arr[index]){
+                swap(arr[i],arr[index]);
+                break;
+            }
+        }
+        reverse(arr.begin()+index+1, arr.end());
+    }
+};
